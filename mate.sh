7737311b42fc399a5beb8646d3c56be3b1017f37
mysterious linux <122890369+mysterious-linux@users.noreply.github.com>
2023-01-19 13:15:24 +0530

    Create mate.sh

---
+++ mate.sh
@@ -0,0 +1,7 @@
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install xfce4 -y
+sudo apt install xscreensaver dbus-x11
+sudo wget -nc https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
+sudo apt-get install simplescreenrecorder -y
+sudo apt install firefox-esr -y
